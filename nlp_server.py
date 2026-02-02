afced7ef3d41eedf63c292f03621a80b7a8310c1
blindsector <blind_sector@protonmail.com>
2026-02-02 17:31:30 +0100

    Update nlp_server.py

--- nlp_server.py
+++ nlp_server.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import stanza
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
+# Изтегляне на модела (става само първия път)
+stanza.download("bg")
+nlp = stanza.Pipeline("bg", processors="tokenize,pos,lemma")
 
 def preserve_case(original, new):
     if original.isupper():
@@ -12,56 +16,73 @@ def preserve_case(original, new):
         return new.capitalize()
     return new
 
-def smart_replace(word, dictionary, reverse=False):
-    endings = ["ите", "ът", "та", "то", "а", "я", "и"]
+def inflect_bg(original_word, new_lemma, feats):
+    """
+    Опростено огъване според граматичните тагове
+    feats пример: Gender=Neut|Number=Sing|Definite=Def
+    """
+    if not feats:
+        return new_lemma
 
-    base_word = word.lower()
+    # Определителен член
+    if "Definite=Def" in feats:
+        if "Gender=Fem" in feats:
+            return new_lemma + "та"
+        elif "Gender=Neut" in feats:
+            return new_lemma + "то"
+        elif "Gender=Masc" in feats:
+            return new_lemma + "ът"
 
-    for ending in endings:
-        if base_word.endswith(ending):
-            root = base_word[:-len(ending)]
-            break
-    else:
-        root = base_word
-        ending = ""
+    # Множествено число
+    if "Number=Plur" in feats:
+        if new_lemma.endswith("а") or new_lemma.endswith("я"):
+            return new_lemma[:-1] + "и"
+        return new_lemma + "и"
 
-    if reverse:
-        reverse_dict = {v: k for k, v in dictionary.items()}
-        if root in reverse_dict:
-            new_root = reverse_dict[root]
-        else:
-            return word
-    else:
-        if root in dictionary:
-            new_root = dictionary[root]
-        else:
-            return word
-
-    new_word = new_root + ending
-    return preserve_case(word, new_word)
+    return new_lemma
 
 def transform_text(text, dictionary, reverse=False):
-    words = text.split()
-    result = [smart_replace(w, dictionary, reverse) for w in words]
-    return " ".join(result)
+    doc = nlp(text)
+    reverse_dict = {v: k for k, v in dictionary.items()}
+    output_words = []
 
-@app.route("/")
-def home():
-    return "Penguin NLP Server Running 🐧"
+    for sentence in doc.sentences:
+        for word in sentence.words:
+            original = word.text
+            lemma = word.lemma.lower()
+            feats = word.feats  # граматични тагове
+
+            if reverse:
+                replacement = reverse_dict.get(lemma)
+            else:
+                replacement = dictionary.get(lemma)
+
+            if replacement:
+                new_word = inflect_bg(original.lower(), replacement, feats)
+                new_word = preserve_case(original, new_word)
+                output_words.append(new_word)
+            else:
+                output_words.append(original)
+
+    return " ".join(output_words)
 
 @app.route("/encode", methods=["POST"])
 def encode():
     data = request.json
-    text = data.get("text", "")
-    dictionary = data.get("dictionary", {})
-    return jsonify({"result": transform_text(text, dictionary)})
+    return jsonify({
+        "result": transform_text(data.get("text", ""), data.get("dictionary", {}))
+    })
 
 @app.route("/decode", methods=["POST"])
 def decode():
     data = request.json
-    text = data.get("text", "")
-    dictionary = data.get("dictionary", {})
-    return jsonify({"result": transform_text(text, dictionary, reverse=True)})
+    return jsonify({
+        "result": transform_text(data.get("text", ""), data.get("dictionary", {}), reverse=True)
+    })
+
+@app.route("/")
+def home():
+    return "Penguin NLP Morph Server 🐧"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
